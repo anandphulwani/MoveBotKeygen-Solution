260a31537400a7b92eb5d7e172396561f4ac22ae
Anand Phulwani <anand_phulwani@yahoo.com>
2024-02-03 15:02:27 +0530

    Created keygen for MoveBot 2020

---
+++ MoveBotKey.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class MoveBotKey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUIC = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxActivationCode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGenerate.Location = new System.Drawing.Point(406, 365);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(140, 54);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your UIC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(119, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Your Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(59, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Activation Code";
+            // 
+            // textBoxUIC
+            // 
+            this.textBoxUIC.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUIC.Location = new System.Drawing.Point(278, 121);
+            this.textBoxUIC.Name = "textBoxUIC";
+            this.textBoxUIC.Size = new System.Drawing.Size(717, 38);
+            this.textBoxUIC.TabIndex = 4;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.Location = new System.Drawing.Point(278, 199);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(717, 38);
+            this.textBoxEmail.TabIndex = 5;
+            // 
+            // textBoxActivationCode
+            // 
+            this.textBoxActivationCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxActivationCode.Location = new System.Drawing.Point(278, 277);
+            this.textBoxActivationCode.Name = "textBoxActivationCode";
+            this.textBoxActivationCode.ReadOnly = true;
+            this.textBoxActivationCode.Size = new System.Drawing.Size(717, 38);
+            this.textBoxActivationCode.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(317, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(543, 55);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MoveBOT Keygenerator";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.buttonReset.Location = new System.Drawing.Point(579, 365);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(157, 54);
+            this.buttonReset.TabIndex = 8;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 450);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxActivationCode);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxUIC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUIC;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxActivationCode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ MoveBotKey.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MoveBotKey : Form
+    {
+        public string CODE_BIT;
+        public byte[] Car_Amm;
+        public static byte[] CODELED = new byte[11];
+
+        public MoveBotKey()
+        {
+            this.CODE_BIT = "00000000000";
+            this.Car_Amm = new byte[0x40];
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int index = 0;
+            while (true)
+            {
+                this.Car_Amm[index] = (byte)(0x41 + index);
+                index++;
+                if (index > 0x19)
+                {
+                    int num2 = 0x1a;
+                    while (true)
+                    {
+                        this.Car_Amm[num2] = (byte)((0x30 + num2) - 0x1a);
+                        num2++;
+                        if (num2 > 30)
+                        {
+                            int num3 = 0x1f;
+                            while (true)
+                            {
+                                this.Car_Amm[num3] = (byte)((0x61 + num3) - 0x1f);
+                                num3++;
+                                if (num3 > 0x38)
+                                {
+                                    int num4 = 0x39;
+                                    while (true)
+                                    {
+                                        this.Car_Amm[num4] = (byte)((0x35 + num4) - 0x39);
+                                        num4++;
+                                        if (num4 > 0x3d)
+                                        {
+                                            this.Car_Amm[0x3e] = 0x2b;
+                                            this.Car_Amm[0x3f] = 0x2d;
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public string ATTIVA_LED(byte indice = 1)
+        {
+            CODELED = new byte[11];
+            this.CODE_BIT = Strings.Format(Conversion.Val(IntToBin((int)Oct2Dec("3777"))), "00000000000");
+            int index = 0;
+            do
+            {
+                CODELED[index] = (byte)Conversion.Val(this.CODE_BIT.ToCharArray()[index]);
+                index++;
+            }
+            while (index <= 10);    
+            return this.CODE_BIT;
+        }
+        
+        public string CODEX_CONTROL()
+        {
+            this.ATTIVA_LED(1);
+            char[] chArray = this.CODEX_CONVERSION_UIC(textBoxUIC.Text).ToCharArray();
+            char[] chArray2 = this.CODEX_CONVERSION_EMAIL(textBoxEmail.Text).ToCharArray();
+            char[] chArray3 = this.CODEX_CONVERSION_ACTIVATION(Strings.Left(this.CODE_BIT, 11)).ToCharArray();
+            string str = "";
+            int index = 7;
+            while (true)
+            {
+                char ch = chArray[index];
+                char ch2 = chArray2[7 - index];
+                char ch3 = chArray3[index];
+                str = str + Strings.Format(((1 + Strings.Asc(ch)) * (1 + Strings.Asc(ch2))) * (1 + Strings.Asc(ch3)), "000000");
+                index += -1;
+                if (index < 0)
+                {
+                    if ((str.Length % 2) != 0)
+                    {
+                        str = str + "0";
+                    }
+                    string str2 = "";
+                    int length = str.Length;
+                    for (int i = 1; i <= length; i += 2)
+                    {
+                        string inputStr = Strings.Mid(str, i, 2);
+                        str2 = (Conversion.Val(inputStr) > 0x3f) ? (str2 + inputStr) : (str2 + Conversions.ToString(Strings.Chr(this.Car_Amm[Conversions.ToInteger(inputStr)])));
+                    }
+                    string str3 = "";
+                    int num4 = 0;
+                    while (true)
+                    {
+                        str3 = str3 + CODELED[num4].ToString();
+                        num4++;
+                        if (num4 > 10)
+                        {
+                            str2 = str2 + ":" + Strings.Format(Conversion.Val(Dec2Oct((long)_BIT_to_BYTE(str3, 11))), "0000");
+                            double num5 = Conversion.Val(Dec2Oct((long)_BIT_to_BYTE(str3, 11)));
+                            // this.Label7.Text = (num5 != 0x179) ? ((num5 != 0x309) ? ((num5 != 0x561) ? ((num5 != 0x949) ? "max 1" : "max 10") : "max 5") : "max 1") : "max 1";
+                            return str2;
+                        }
+                    }
+                }
+            }
+        }
+
+        public string CODEX_CONVERSION_ACTIVATION(string ACTIVATION)
+        {
+            long expression = 0L;
+            int num2 = 0;
+            foreach (char ch in ACTIVATION.ToCharArray())
+            {
+                num2 += 0x400;
+                expression += Strings.Asc(ch) * num2;
+            }
+            return Strings.Format(expression, "00000000");
+        }
+
+        public string CODEX_CONVERSION_EMAIL(string EMAIL)
+        {
+            if (EMAIL.Length > 0x20)
+            {
+                EMAIL = EMAIL.Substring(1, 0x20);
+            }
+            long expression = 0L;
+            int num2 = 0;
+            foreach (char ch in EMAIL.ToCharArray())
+            {
+                num2 += 0x100;
+                expression += Strings.Asc(ch) * num2;
+            }
+            return Strings.Format(expression, "00000000");
+        }
+
+        public string CODEX_CONVERSION_UIC(string UIC)
+        {
+            long expression = 0L;
+            UIC = UIC.Replace("-", "");
+            int num2 = 0;
+            foreach (char ch in UIC.ToCharArray())
+            {
+                num2 += 0x100;
+                expression += Strings.Asc(ch) * num2;
+            }
+            return Strings.Format(expression, "00000000");
+        }
+
+        public static ulong _BIT_to_BYTE(string _BitSeries, byte Num_Bit)
+        {
+            ulong num1 = 0;
+            if (_BitSeries.Length > (int)Num_Bit)
+                _BitSeries = Microsoft.VisualBasic.Strings.Left(_BitSeries, (int)Num_Bit);
+            int num2 = (int)Num_Bit - 1;
+            for (int y = 0; y <= num2; ++y)
+            {
+                if ((byte)Math.Round(Conversion.Val(Microsoft.VisualBasic.Strings.Mid(_BitSeries, y + 1, 1))) != (byte)0)
+                    num1 = (ulong)Math.Round((double)num1 + Math.Pow(2.0, (double)y));
+            }
+            return num1;
+        }
+
+        public static string Dec2Oct(long Decimal_Number)
+        {
+            StackTrace stackTrace = new StackTrace();
+            string Name_Function = "{" + Microsoft.VisualBasic.Strings.Replace(stackTrace.GetFrame(0).GetMethod().ReflectedType.ToString(), ".", "->") + "->" + stackTrace.GetFrame(0).GetMethod().Name.ToString() + "}";
+            string str = "";
+            try
+            {
+                Decimal_Number = Math.Abs(Decimal_Number);
+                do
+                {
+                    str = Conversions.ToString(Decimal_Number % 8L) + str;
+                    Decimal_Number /= 8L;
+                    Application.DoEvents();
+                }
+                while (Decimal_Number > 0L);
+            }
+            catch (Exception ex)
+            {
+                ProjectData.SetProjectError(ex);
+                ProjectData.ClearProjectError();
+            }
+            return str;
+        }
+
+        public static long Oct2Dec(string Octal_Number)
+        {
+            StackTrace stackTrace = new StackTrace();
+            string Name_Function = "{" + Microsoft.VisualBasic.Strings.Replace(stackTrace.GetFrame(0).GetMethod().ReflectedType.ToString(), ".", "->") + "->" + stackTrace.GetFrame(0).GetMethod().Name.ToString() + "}";
+            long num = 0;
+            try
+            {
+                for (int Start = Microsoft.VisualBasic.Strings.Len(Octal_Number); Start >= 1; Start += -1)
+                    num = (long)Math.Round((double)Conversions.ToInteger(Microsoft.VisualBasic.Strings.Mid(Octal_Number, Start, 1)) * Math.Pow(8.0, (double)(Octal_Number.Length - Start)) + (double)num);
+            }
+            catch (Exception ex)
+            {
+                ProjectData.SetProjectError(ex);
+                ProjectData.ClearProjectError();
+            }
+            return num;
+        }
+
+        public static string IntToBin(int filevalue)
+        {
+            BitArray bitArray = new BitArray(new int[1]
+            {
+        filevalue
+            });
+            string str = "";
+            int num = bitArray.Count - 1;
+            for (int index = 0; index <= num; ++index)
+                str = bitArray[index] ? str + "1" : str + "0";
+            return Microsoft.VisualBasic.Strings.Left(str, 11);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxActivationCode.Text = this.CODEX_CONTROL();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            textBoxUIC.Text = "";
+            textBoxEmail.Text = "";
+            textBoxActivationCode.Text = "";
+        }
+    }
+}
